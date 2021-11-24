6a33b008e50bf81d65f5b439e0103f7b6ecb1b44
Lyso95 <amos@langensiepen.net>
2021-11-24 14:09:00 +0100

    js_finished, placeholderimg

--- js/site.js
+++ js/site.js
@@ -1,4 +1,37 @@
-//Call Hello World
-function helloWorld(){
-    alert("Hello World");
+//Get the string from the page
+//controller
+function getValue(){
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;  
+    let revString = checkPalindrome(userString);
+    displayString(revString);
+}
+//Reverse the string
+//model
+function checkPalindrome(userString) {
+    let revString = [];
+    let stringEven = false;
+    let reversedString = "";
+    
+    //reverse a string using a for loop
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+    if(revString.localeCompare(userString)){
+        stringEven = true;
+        revString = 'Text ist kein Palindrom!'
+    } else {
+        stringEven = false;
+    }
+    return revString;
+}
+
+//Display the reversed string to the user
+//view
+function displayString(revString){
+    //write to the page
+    document.getElementById("msg").innerHTML = `Dein Text rückwärts lautet: ${revString}`; 
+    //turn on alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
